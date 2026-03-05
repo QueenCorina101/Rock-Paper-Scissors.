b878ccafbdb3991fc9c9d9dc43f860fc5e4c951e
QueenCorina101 <clarkcorina1@gmail.com>
2026-03-05 11:45:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,22 @@ user_score = 0
 computer_score = 0
 
 while True:
-  user_choice = input ("Enter rock, papder, or scissors (or type 'quit' to stop):").lower()
+  user_choice = input ("Enter rock, paper, or scissors (or type 'quit' to stop):").lower()
+  if user_choice == "quit":
+    break
+  if user_choice not in choices:
+    print("Invalid choice. Try again.")
+    continue
+  computer_choice = random.choice(choices)
+  print("Computer chose:", computer_choice)
+  if user_choice == computer_choice:
+    print("Your close, it's a tie!")
+  elif (user_choice == "rock" and computer_choice == "scissors") or \
+  (user_choice == "paper" and computer_choice == "rock") or \
+  (user_choice == "scissors" and computer_choice == "paper"):
+    print("You win!")
+    user_score += 1
+  else:
+    print("You lose!")
+    computer_score += 1
+
