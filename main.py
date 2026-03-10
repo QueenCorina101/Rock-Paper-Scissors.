47d7b3eba5d1f381347c4c746c36426ca1e79751
QueenCorina101 <clarkcorina1@gmail.com>
2026-03-10 10:08:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 print("Rock, Paper, Scissors Game!")
 
 choices = ["rock", "paper", "scissors"]
-user_score = 0
-computer_score = 0
+user_score = 1-10
+computer_score = 1-10
 
 while True:
   user_choice = input ("Enter rock, paper, or scissors (or type 'quit' to stop):").lower()
